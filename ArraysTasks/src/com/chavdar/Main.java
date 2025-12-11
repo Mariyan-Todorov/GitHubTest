22efb4ca0bd0ac888c35989c00fe1200e9adab67
Marian Todorov <marian.sf.2906@gmail.com>
2025-12-11 20:26:06 +0200

    Update ArraysTasks project

--- ArraysTasks/src/com/chavdar/Main.java
+++ ArraysTasks/src/com/chavdar/Main.java
@@ -2,6 +2,8 @@ package com.chavdar;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello Git!");
+
+        System.out.println("Now I can continue make changes to my project :)");
     }
 }
